f41c6e34b017d5cbeb1f8b131dc99e1612bfac0a
Evert <imsonotsleepy@gmail.com>
2017-03-17 14:22:18 +0200

    17-03-17-14:22:18

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,6 @@
 let term = {
   promptLen: 0,
+  returnCode: 0,
   focused: true,
   promptDisabled: true
 }
@@ -19,7 +20,7 @@ function getURLs (s) {
   return s.match(regexp)
 }
 
-function chunkify (text) {
+function chunkify (text, selectedChars) {
   let chars = text.split('')
   let endHtml = ''
   let observingCtrl = null
@@ -39,7 +40,7 @@ function chunkify (text) {
 
   for (let i in chars) {
     let char = chars[i]
-    let hexVal = hexEncode(char)
+    let extr = hexEncode(char)
 
     if (char === '\x1b') {
       observingCtrl = true
@@ -49,9 +50,15 @@ function chunkify (text) {
     if (observingCtrl === true && (!isNaN(parseInt(char)) || char.match(/[a-f]/))) {
       observingCtrl = isNaN(parseInt(char)) ? char.match(/[a-f]/)[0] : parseInt(char)
       continue
-    } else if (observingCtrl && char === 'r') {
+    } else if (observingCtrl === true && char === 'r') {
       observingCtrl = null
       continue
+    }
+
+    if (selectedChars != null) {
+      if (i >= selectedChars[0] && i <= selectedChars[1]) {
+        extr += ' sel'
+      }
     }
 
     for (let j in urlsAt) {
@@ -66,7 +73,7 @@ function chunkify (text) {
     if (char === '\n') {
       endHtml += '<br>'
     } else {
-      endHtml += '<span class="char char-' + hexVal + (observingCtrl != null ? ' col-' + observingCtrl : '') + '">' + char + '</span>'
+      endHtml += '<span class="char char-' + extr + (observingCtrl != null ? ' col-' + observingCtrl : '') + '">' + char + '</span>'
     }
   }
 
@@ -77,14 +84,26 @@ function clonePrompt () {
   let fakePrompt = term.prompt.cloneNode(true)
   fakePrompt.removeChild(fakePrompt.querySelector('.cursor'))
   fakePrompt.removeChild(fakePrompt.querySelector('#pif'))
-  fakePrompt.className = 'old_prompt'
+  fakePrompt.className = 'prompt old'
   fakePrompt.removeAttribute('id')
   term.lb.appendChild(fakePrompt)
+  scrollBottom()
 }
 
 function writeOut (text, cb) {
   term.lb.innerHTML += chunkify(text)
   if (typeof cb === 'function') cb()
+}
+
+function writeOutDelayed (text, delayms, cb) {
+  setTimeout(() => {
+    writeOut(text, cb)
+  }, delayms)
+}
+
+function scrollBottom () {
+  document.body.scrollTop = document.body.scrollHeight
+  document.documentElement.scrollTop = document.body.scrollHeight
 }
 
 function spellOut (text, delayms, cb) {
@@ -102,7 +121,7 @@ function spellOut (text, delayms, cb) {
     if (observingCtrl === true && (!isNaN(parseInt(char)) || char.match(/[a-f]/))) {
       observingCtrl = isNaN(parseInt(char)) ? char.match(/[a-f]/)[0] : parseInt(char)
       return
-    } else if (observingCtrl && char === 'r') {
+    } else if (observingCtrl === true && char === 'r') {
       observingCtrl = null
       return
     }
@@ -113,7 +132,6 @@ function spellOut (text, delayms, cb) {
       term.lb.innerHTML += '<span class="char char-' + hexVal + (observingCtrl != null ? ' col-' + observingCtrl : '') + '">' + char + '</span>'
     }
 
-    updateCursor()
     if (i + 1 === chars.length && typeof cb === 'function') cb()
   }
 
@@ -128,23 +146,53 @@ function spellOut (text, delayms, cb) {
 function updateCursor () {
   // CALCULATE POS
 
-  let len = 0
-  len += term.promptLen
+  let len = term.promptLen
 
   let charExample = document.querySelector('.char')
-  let blockLen = 8
+  let blockLen = charExample.offsetWidth
+  let blockHei = charExample.offsetHeight
 
+  let promptLen = term.prompt.clientWidth
+
+  // Get percise measurements if available
   if (charExample.getBoundingClientRect) {
     blockLen = charExample.getBoundingClientRect().width
-  } else {
-    blockLen = charExample.offsetWidth
+    blockHei = charExample.getBoundingClientRect().height
+    promptLen = term.prompt.getBoundingClientRect().width
   }
 
   let posInInput = term.input.selectionStart
+
+  // Tell chunkifier if the user has highlighted some text in the input
+  let selection = null
+  if (term.input.selectionStart !== term.input.selectionEnd) {
+    selection = [term.input.selectionStart, term.input.selectionEnd]
+  }
+
+  // Calculations..
+  let availableWidth = promptLen - (posInInput + len) * blockLen
+  let cursorMoveDown = (availableWidth < blockLen ? blockHei : 0)
   let offsetLeft = (posInInput + len) * blockLen
 
+  // Do not show exit code when theres text behind it
+  if (offsetLeft > promptLen - blockLen * (4 + term.returnCode.toString().length)) {
+    term.return.style.display = 'none'
+  } else if (term.returnCode !== 0) {
+    showReturn(term.returnCode)
+  }
+
+  // Move cursor down
+  if (cursorMoveDown > 0) {
+    offsetLeft = offsetLeft - (promptLen + len) + blockLen
+    if (offsetLeft > promptLen - blockLen) {
+      offsetLeft = offsetLeft - promptLen
+      cursorMoveDown += blockHei
+    }
+  }
+
   term.cursor.style.left = offsetLeft + 'px'
-  term.fakeinput.innerHTML = chunkify(term.input.value)
+  term.cursor.style.top = cursorMoveDown + 'px'
+  term.fakeinput.innerHTML = chunkify(term.input.value, selection)
 
   // SET CLASS
   let baseClass = 'cursor char'
@@ -177,10 +225,24 @@ function setPromptVisible (val) {
   if (val) focus()
 }
 
+function clear () {
+  term.lb.innerHTML = ''
+}
+
 function focus () {
   term.input.focus()
   term.focused = true
   updateCursor()
+}
+
+function showReturn (num) {
+  term.returnCode = num
+  if (num === 0) {
+    term.return.style.display = 'none'
+  } else {
+    term.return.style.display = 'block'
+    term.return.innerHTML = num + ' &crarr;'
+  }
 }
 
 function doInput () {
@@ -197,15 +259,24 @@ window.onload = () => {
   term.ps1 = prompt.querySelector('#ps1')
   term.fakeinput = prompt.querySelector('#fakeinput')
   term.cursor = prompt.querySelector('.cursor')
+  term.return = prompt.querySelector('.returnCode')
   const input = term.input = document.querySelector('#pif')
   setPromptVisible(false)
+  writeOutDelayed('\x1b8Hello there!\n\n', 100)
+  writeOutDelayed('loading system\n\n', 110)
+  writeOutDelayed('exec \'sh\'\n', 500)
 
-  spellOut('Welcome to \x1balunasqu.ee\x1br!\n', 100, () => {
-    writeOut('test \x1b8data\x1br here\nlel\n')
+  writeOutDelayed('\n\n', 1000, () => {
+    clear()
+    spellOut('Welcome to \x1balunasqu.ee\x1br!\n\n', 60, () => {
+      writeOutDelayed('This is an open-source unix-like fake terminal (https://github.com/LunaSquee/lunasqu.ee-term)\n\n', 100)
+      writeOutDelayed('\xA9 2017 LunaSquee\n\n', 200, () => {
+        setPromptVisible(true)
 
-    setPromptVisible(true)
-
-    updatePrompt('\x1b3/home\x1br $ ')
+        showReturn(1)
+        updatePrompt('\x1b3/home\x1br $ ')
+      })
+    })
   })
 
   document.querySelector('body').addEventListener('click', (e) => {
@@ -235,7 +306,6 @@ window.onload = () => {
   }
 
   window.onresize = () => {
-    term.focused = false
     updateCursor()
   }
 
